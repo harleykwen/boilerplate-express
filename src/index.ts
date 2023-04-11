2e847f0882438bdf90a2f75da29872ab926cb12a
harleykwen <harleykwen.work@gmail.com>
2023-04-11 12:52:17 +0700

    Initialize boilerplate

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express, { 
+    Express, 
+    Request, 
+    Response, 
+    NextFunction,
+} from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+ 
+const app: Express = express()
+const port = process.env.PORT
+
+app.use((req: Request, res: Response, next: NextFunction) => { // Handle error CORS policy
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+ 
+app.get('/', (req: Request, res: Response) => {
+    return res?.status(200)?.json({
+        status: 200,
+        error: false,
+        data: null,
+        message: 'Hello World!',
+    })
+})
+ 
+app.listen(port, () => {
+    return console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+})
